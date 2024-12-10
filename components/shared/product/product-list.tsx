6f0f9a5a485641b1848a1c6bb6a60bf38582886e
persipica <likekhs0107@naver.com>
2024-12-10 11:26:24 +0900

    1st

--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 
 import ProductCard from './product-card'
+import { Product } from '@/types'
 
-const ProductList = ({ data }: { data: any }) => {
+const ProductList = ({ data }: { data: Product[] }) => {
   return (
     <>
       {data.length > 0 ? (
         <div>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-            {data.map((product: any) => (
+            {data.map((product: Product) => (
               <ProductCard key={product.slug} product={product} />
             ))}
           </div>
